fff4c9e361a1dc99c56f3c0cafa8ab6ee7c23d3f
AdapaAkhil <adapaakhil6@gmail.com>
2022-03-20 08:39:00 +0530

    threads

---
+++ Assignment4/src/threadassign/FIleDownladerMain.java
@@ -0,0 +1,14 @@
+package threadassign;
+import java.util.*;
+public class FIleDownladerMain {
+	public static void main(String[] args) {
+		List<String> photoUrls= FileUtil.readFile("download.txt");
+		for(String url: photoUrls) {
+			FileDownloadTask task=new FileDownloadTask(url);
+			Thread thread=new Thread(task);
+			thread.start();
+			
+		}
+	}
+
+}
